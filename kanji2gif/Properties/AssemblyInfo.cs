2a7c438c95c93e9a04b324db4cde93422de898dc
shuuryou <36278767+shuuryou@users.noreply.github.com>
2022-03-28 10:13:27 +0200

    New AssemblyInfo.cs file
    Recreated AssemblyInfo with copyright and version updated.

---
+++ kanji2gif/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Kanji2GIF")]
+[assembly: AssemblyDescription("Kanji2GIF Converter")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kanji2gif")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a072de5c-9086-4d6f-b501-5617a15db179")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
